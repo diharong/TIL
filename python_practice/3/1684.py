d4bc41ada76d2c859337d4d6cf0460b999dd5107
diharong <diharong01@gmail.com>
2024-08-11 22:14:41 +0900

    dd

--- python_practice/3/1684.py
+++ python_practice/3/1684.py
@@ -1,37 +1,36 @@
-# 실습 번호.py
-number_of_people = 0
+# # 실습 번호.py
+# number_of_people = 0
 
-def increase_user():
-    global number_of_people
-    number_of_people += 1
+# def increase_user():
+#     global number_of_people
+#     number_of_people += 1
 
-number_of_book = 100
+# number_of_book = 100
 
-def decrease_book(number):
-    global number_of_book
-    number_of_book -= number
-    print(f'남은 책의 수 : {number_of_book}')
-
-
-name = ['김시습', '허균', '남영로', '임제', '박지원']
-age = [20, 16, 52, 36, 60]
+# def decrease_book(number):
+#     global number_of_book
+#     number_of_book -= number
+#     print(f'남은 책의 수 : {number_of_book}')
 
 
-
-def create_user():
-    increase_user()
-    user_info = {}
-    user_info['name'] = name       
-    user_info['age'] = age
-    print(f'{name}님 환영합니다!')
-    return user_info
+# name = ['김시습', '허균', '남영로', '임제', '박지원']
+# age = [20, 16, 52, 36, 60]
 
 
-many_user = list(map(create_user, name, age))
+
+# def create_user():
+#     increase_user()
+#     user_info = {}
+#     user_info['name'] = name       
+#     user_info['age'] = age
+#     print(f'{name}님 환영합니다!')
+#     return user_info
 
 
-info = list(map(lambda a:{'name':a['name'],'age' : a['age']} , many_user))
+# many_user = list(map(create_user, name, age))
 
 
-def rental_book(info):
-    
+# info = list(map(lambda a:{'name':a['name'],'age' : a['age']} , many_user))
+
+
+# def rental_book(info):
