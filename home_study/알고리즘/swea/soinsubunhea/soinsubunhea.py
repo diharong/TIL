14120a4ccda7b68241512ce64f7a533b6865389b
diharong <diharong01@gmail.com>
2024-08-16 00:04:59 +0900

    dd

---
+++ home_study/알고리즘/swea/soinsubunhea/soinsubunhea.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+
+
+    result = []
+
+    if N%2 == 0:
+        result.append()
+
